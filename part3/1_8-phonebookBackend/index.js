a296f5828ff50c125551c18b8f29ef795be71847
Hiep Hoang <85355255+HiepHoang132@users.noreply.github.com>
2025-05-30 15:38:54 +0700

    Refactor: add Person schema, use env URI, remove mongo.js

--- part3/1_8-phonebookBackend/index.js
+++ part3/1_8-phonebookBackend/index.js
@@ -1,5 +1,8 @@
+require('dotenv').config()
+
 const express = require('express')
 const morgan = require('morgan');
+const Person = require('./models/person')
 
 const app = express()
 
@@ -7,7 +10,7 @@ app.use(express.json())
 app.use(express.static('dist'))
 
 // Custom token to get request body
-morgan.token('body', (req, res) => {
+morgan.token('body', req => {
      return JSON.stringify(req.body)
 })
 
@@ -15,69 +18,34 @@ morgan.token('body', (req, res) => {
 const morganFormat = ':method :url :status :res[content-length] - :response-time ms :body'
 app.use(morgan(morganFormat))
 
-let persons = [
-    {
-        "id": "1",
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": "2",
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": "3",
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": "4",
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
-
-// 3.1
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    Person.find({}).then(persons => {
+        res.json(persons)
+    })
 })
 
-// 3.2
 app.get('/info', (req, res) => {
-    const body = `
-        <p>Phonebook has info for ${persons.length} people</p>
-        <p>${new Date().toString()}</p>
-    `
-    res.send(body)
+    Person.find({}).then(persons => {
+        const body = `
+            <p>Phonebook has info for ${persons.length} people</p>
+            <p>${new Date().toString()}</p>
+        `
+        res.send(body)
+    })
 })
 
-// 3.3
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
     const id = req.params.id
-    const person = persons.find(p => p.id === id)
 
-    if(!person){
+    Person.findById(id).then(person => {
+        if(person){
+            return res.json(person)
+        }
+
         res.statusMessage = `Not found person with id ${id} `
-        return res.status(404).end()
-    }
-
-    res.json(person)
+        res.status(404).end()
+    }).catch(error => next(error))
 })
-
-// 3.4
-app.delete('/api/persons/:id', (req, res) => {
-    const id = req.params.id
-    persons = persons.filter(p => p.id !== id)
-
-    res.status(204).end()
-})
-
-// 3.5
-const generateId = () => {
-    const random = Math.floor(Math.random() * 100000)
-    return String(random)
-}
 
 app.post('/api/persons', (req, res) => {
     const body = req.body
