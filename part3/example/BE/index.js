56e3685451d707ef3765d8026643bbbd7043cbe9
Hiep Hoang <85355255+HiepHoang132@users.noreply.github.com>
2025-05-30 14:30:27 +0700

    Refactor: integrate MongoDB for notes create, read

--- part3/example/BE/index.js
+++ part3/example/BE/index.js
@@ -1,44 +1,34 @@
+require('dotenv').config()
+
 const express = require('express')
+const morgan = require('morgan')
+const Note = require("./models/note");
 
 const app = express()
 
 app.use(express.json())
 app.use(express.static('dist'))
 
-const requestLogger = (request, response, next) => {
-    console.log('Method:', request.method)
-    console.log('Path:  ', request.path)
-    console.log('Body:  ', request.body)
-    console.log('---')
-    next()
-}
+// const requestLogger = (request, response, next) => {
+//     console.log('Method:', request.method)
+//     console.log('Path:  ', request.path)
+//     console.log('Body:  ', request.body)
+//     console.log('---')
+//     next()
+// }
 
-app.use(requestLogger)
+// app.use(requestLogger)
 
-let notes = [
-    {
-        id: "1",
-        content: "HTML is easy",
-        important: true
-    },
-    {
-        id: "2",
-        content: "Browser can execute only JavaScript",
-        important: false
-    },
-    {
-        id: "3",
-        content: "GET and POST are the most important methods of HTTP protocol",
-        important: true
-    }
-]
+app.use(morgan('tiny'))
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
@@ -97,7 +87,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
