e4b4f4e9b6b9bed5bb572dc39c57b37700673acc
Hiep Hoang <85355255+HiepHoang132@users.noreply.github.com>
2025-05-30 15:41:21 +0700

    Refactor: remove requestLogger

--- part3/example/BE/index.js
+++ part3/example/BE/index.js
@@ -9,16 +9,6 @@ const app = express()
 app.use(express.json())
 app.use(express.static('dist'))
 
-// const requestLogger = (request, response, next) => {
-//     console.log('Method:', request.method)
-//     console.log('Path:  ', request.path)
-//     console.log('Body:  ', request.body)
-//     console.log('---')
-//     next()
-// }
-
-// app.use(requestLogger)
-
 app.use(morgan('tiny'))
 
 app.get('/', (request, response) => {
