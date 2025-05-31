eaca4c9fabd50abb1ead29383a3859878430b4ad
Hiep Hoang <85355255+HiepHoang132@users.noreply.github.com>
2025-05-31 13:19:52 +0700

    Add note validation and error handling

--- part3/example/BE/index.js
+++ part3/example/BE/index.js
@@ -35,23 +35,17 @@ app.get('/api/notes/:id', (request, response, next) => {
     }).catch(error => next(error))
 })
 
-app.post('/api/notes', (req, res) => {
-    const body = req.body
-
-    if(!body.content){
-        return res.status(400).json({
-            error: 'content missing'
-        })
-    }
+app.post('/api/notes', (req, res, next) => {
+    const {content, important}= req.body
 
     const note = new Note({
-        content: body.content,
-        important: body.important || false,
+        content: content,
+        important: important || false,
     })
 
    note.save().then(newNote => {
        res.json(newNote)
-   })
+   }).catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (req, res, next) => {
@@ -92,6 +86,8 @@ const errorHandler = (error, req, res, next) => {
 
     if(error.name === 'CastError'){
         return res.status(400).send({error: "malformatted id"})
+    } else if(error.name === 'ValidationError'){
+        return res.status(400).json({ error: error.message })
     }
 
     next(error)
