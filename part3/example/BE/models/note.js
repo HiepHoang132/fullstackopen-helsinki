4cc4ea55b9fa0d23cb7057e509aa3b6fbce88286
Hiep Hoang <85355255+HiepHoang132@users.noreply.github.com>
2025-06-02 13:12:57 +0700

    Refactor project structure: separate controllers, app.js, and utils

--- part3/example/BE/models/note.js
+++ part3/example/BE/models/note.js
@@ -1,24 +1,12 @@
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false)
-
-const url = process.env.MONGODB_URI
-
-console.log('Connecting to', url)
-
-mongoose.connect(url)
-  .then(() => {
-    console.log('Connected to MongoDB')
-  })
-  .catch(error => console.log('Error connecting to MongoDB:', error.message))
-
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
-    minLength: 5,
-    required: true
+    required: true,
+    minlength: 5
   },
-  important: Boolean
+  important: Boolean,
 })
 
 noteSchema.set('toJSON', {
@@ -29,6 +17,4 @@ noteSchema.set('toJSON', {
   }
 })
 
-const Note = mongoose.model('Note', noteSchema)
-
-module.exports = Note
+module.exports = mongoose.model('Note', noteSchema)
---
+++ part3/example/BE/utils/middleware.js
@@ -0,0 +1,20 @@
+const logger = require('logger')
+
+const unknownEndpoint = (req, res) => {
+  return res.status(404).end()
+}
+
+const errorHandler = (error, req, res, next) => {
+  logger.info(error.message())
+
+  if(error.name === 'CastError'){
+    return res.status(400).json({ error: 'malformatted id' })
+  } else if(error.name === 'ValidationError'){
+    const messages = Object.values(error.errors).map(e => e.message)
+    return res.status(400).json({ error: messages.join(',') })
+  }
+
+  next(error)
+}
+
+module.exports = { unknownEndpoint, errorHandler }
