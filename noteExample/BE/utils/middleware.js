6d4dcb055328578ff396a3de4ac37c3216ce8e87
Hiep Hoang <85355255+HiepHoang132@users.noreply.github.com>
2025-06-06 10:52:15 +0700

    Add Mongoose schema for users and implement user creation endpoint

--- noteExample/BE/utils/middleware.js
+++ noteExample/BE/utils/middleware.js
@@ -9,11 +9,12 @@ const errorHandler = (error, req, res, next) => {
 
   if(error.name === 'CastError'){
     return res.status(400).json({ error: 'malformatted id' })
-  }
-
-  if(error.name === 'ValidationError'){
+  } else if(error.name === 'ValidationError'){
     const messages = Object.values(error.errors).map(e => e.message)
     return res.status(400).json({ error: messages.join(',') })
+  } else if(error.name === 'MongoServerError'
+      && error.message.includes('E11000 duplicate key error')){
+    return res.status(400).json({ error: 'expected `username` to be unique' })
   }
 
   next(error)
