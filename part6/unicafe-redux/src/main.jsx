8b4501f019ac6e13b4b8f1750a4186f70e1fbb32
Hiep Hoang <85355255+HiepHoang132@users.noreply.github.com>
2025-08-11 16:24:14 +0700

    add part 6.1 - 6.2: Unicafe Revisited

---
+++ part6/unicafe-redux/src/main.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import reducer from './reducer'
+import {configureStore} from "@reduxjs/toolkit";
+
+const store = configureStore({
+    reducer: {
+        counter: reducer
+    }
+})
+
+const App = () => {
+    const good = () => {
+        store.dispatch({
+            type: 'GOOD'
+        })
+    }
+
+    const ok = () => {
+        store.dispatch({
+            type: 'OK'
+        })
+    }
+
+    const bad = () => {
+        store.dispatch({
+            type: 'BAD'
+        })
+    }
+
+    const zero = () => {
+        store.dispatch({
+            type: 'ZERO'
+        })
+    }
+
+    return (
+        <div>
+            <button onClick={good}>good</button>
+            <button onClick={ok}>ok</button>
+            <button onClick={bad}>bad</button>
+            <button onClick={zero}>reset stats</button>
+            <div>good {store.getState().counter.good}</div>
+            <div>ok {store.getState().counter.ok}</div>
+            <div>bad {store.getState().counter.bad}</div>
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const renderApp = () => {
+    root.render(<App/>)
+}
+
+renderApp()
+store.subscribe(renderApp)
