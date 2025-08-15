ec0f6162394b13429ae9fcdad440698ac5ebc407
Hiep Hoang <85355255+HiepHoang132@users.noreply.github.com>
2025-08-15 22:25:38 +0700

    refactor(noteReducer): use createSlice to simplify reducer and actions

--- redux/src/reducers/noteReducer.js
+++ redux/src/reducers/noteReducer.js
@@ -1,4 +1,4 @@
-import {createSelector} from "@reduxjs/toolkit";
+import {createSelector, createSlice} from "@reduxjs/toolkit";
 
 const initialState = [
     {
@@ -13,13 +13,20 @@ const initialState = [
     },
 ]
 
-const noteReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case 'NEW_NOTE':
-            // return state.concat(action.payload)
-            return [...state, action.payload]
-        case 'TOGGLE_IMPORTANCE': {
-            const id = action.payload.id
+const noteSlice = createSlice({
+    name: 'notes',
+    initialState,
+    reducers: {
+        createNote(state, action) {
+            const content = action.payload
+            state.push({
+                content: content,
+                important: false,
+                id: generateId()
+            })
+        },
+        toggleImportanceOf(state, action) {
+            const id = action.payload
             const noteToChange = state.find(note => note.id === id)
             const changedNote = {
                 ...noteToChange,
@@ -27,38 +34,16 @@ const noteReducer = (state = initialState, action) => {
             }
             return state.map(note => note.id !== id ? note : changedNote)
         }
-        default:
-            return state
     }
-}
+})
 
 const generateId = () =>
     Number((Math.random() * 1000000).toFixed(0))
 
-export const createNote = (content) => {
-    return {
-        type: 'NEW_NOTE',
-        payload: {
-            content,
-            important: false,
-            id: generateId()
-        }
-    }
-}
-
-export const toggleImportanceOf = (id) => {
-    return {
-        type: 'TOGGLE_IMPORTANCE',
-        payload: {id}
-    }
-}
-
-const selectNotes = state => state.note
-
 export const selectImportantNotes = createSelector(
-    [selectNotes],
+    state => state.note,
     notes => notes.filter(n => n.important)
 )
 
-
-export default noteReducer;
+export const {createNote, toggleImportanceOf} = noteSlice.actions
+export default noteSlice.reducer
